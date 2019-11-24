4e1e3734294a0b3a4014e139026dc1330d1123a1
cevheryilmaz <cevheryilmaz96@gmail.com>
2019-11-24 13:02:06 +0300

    Service application Service completed

---
+++ api/controllers/todoListController.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+var mongoose = require('mongoose'),
+  Task = mongoose.model('Tasks');
+
+  //list_all_tasks Function
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+
+//create_a_task Function
+exports.create_a_task = function(req, res) {
+  var new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+//read_a_task Function
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+//update_a_task Function
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+//delete_a_task Function
+exports.delete_a_task = function(req, res) {
+
+
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
